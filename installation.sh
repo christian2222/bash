e510bea66c83b0f739276ebf479bee2b35da228e
xy <damsinner@gmx.de>
2019-04-19 20:58:34 +0200

    installation of vimlatex

--- installation.sh
+++ installation.sh
@@ -3,4 +3,24 @@ apt-get install dvbcut
 apt-get install devede
 apt-get install vim
 apt-get install vim-gtk
+apt-get install openjdk-11-jdk
+apt-get install gparted
+apt-get install texlive-base
+
+cd ~/Downloads
+
+cd ~
+wget https://sourceforge.net/projects/vim-latex/files/releases/vim-latex-1.10.0.tar.gz
+tar xvfz vim-latex-1.10.0.tar.gz
+mv vim-latex-1.10.0 .vim
+
+cd ~
+git clone https://github.com/christian2222/dotfiles
+git clone https://github.com/christian2222/bash
+
+cd ~/dotfiles
+./makeLinks.sh
+
+cd ~
+ll
 

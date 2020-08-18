dc63ed0ef3bf1fadeb51d54632c7debc9dfb23c9
xy <damsinner@gmx.de>
2020-08-18 10:14:17 +0200

    better adding files

--- addFiles.sh
+++ addFiles.sh
@@ -1,3 +1,4 @@
-git add \*.* installation/\*.* installation/subroutines/*.sh directoryWork/\*.sh toMp3/\*.sh directoryWork/slow/\*.sh other/\*.sh
+# git add \*.* installation/\*.* installation/subroutines/*.sh directoryWork/\*.sh toMp3/\*.sh directoryWork/slow/\*.sh other/\*.sh
+git add --all
 git commit
 git push

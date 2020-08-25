dce4e6075d8aa0657950bf8f346c04f51e40b56c
xy <damsinner@gmx.de>
2020-08-25 17:31:54 +0200

    more notes and a pv copy

---
+++ tools/copyOld.sh
@@ -0,0 +1,7 @@
+pv ./important18102015Newer/programmingMobile/javaMobile.tar > ./1.tar
+pv ./important/programmingMobile/javaMobile.tar > ./2.tar
+pv ./important15102015/programmingMobile/javaMobile.tar > ./3.tar
+pv ./important24102015Newer/programmingMobile/javaMobile.tar > ./4.tar
+pv ./important24112015/programmingMobile/javaMobile.tar > ./5.tar
+pv ./important26112015/programmingMobile/javaMobile.tar > ./6.tar
+pv ./important31102015/programmingMobile/javaMobile.tar > ./7.tar

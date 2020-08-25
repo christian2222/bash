f1383bdd855e51643060687836bba14730a4bb0a
xy <damsinner@gmx.de>
2020-08-25 12:50:10 +0200

    better output for unTarAll.sh

--- tools/unTarAll.sh
+++ tools/unTarAll.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 for file in *.tar
 do
+	echo ""
 	echo "Extracting $file ..."
 	# pv "$file" | tar -x --ignore-failed-read --totals
 	pv "$file" | tar -x --totals
@@ -8,4 +9,5 @@ do
 	if [ $result -eq 0 ]; then
 		echo "File $file successfully extracted."
 	fi
+	echo ""
 done

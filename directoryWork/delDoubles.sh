f0fea77dd6b27836b6a4d0d8161475c5e4c570f0
xy <damsinner@gmx.de>
2020-08-25 23:10:33 +0200

    new notes and problematic untar

--- directoryWork/delDoubles.sh
+++ directoryWork/delDoubles.sh
@@ -1,6 +1,11 @@
 # delete all .double files produced by [compareDirs.sh] or [compareFilesBySize.sh]
 saveIFS=$IFS
 IFS=$(echo -en "\n\b")
+# breche mit Meldung ab falls zuwenig Parameter
+if [ $# -lt 1 ]; then
+	echo "usage: $0 directory"
+	exit 1
+fi
 # lösche alle .double Dateien in $2
 find $1 -name "*.double" -delete
 # lösche leere Verzeichnisse rekursiv beginnend bei den Blättern
---
+++ tools/unTarProblematic.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+for file in *.tar
+do
+	echo ""
+	echo "Extracting $file ..."
+	pv "$file" | tar -x --ignore-failed-read --totals
+	# pv "$file" | tar -x --totals
+	result=$?
+	if [ $result -eq 0 ]; then
+		echo "File $file successfully extracted."
+	fi
+	echo ""
+done

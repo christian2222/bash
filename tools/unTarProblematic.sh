754e41efeb59b5969cd96ec0a85c9eb1cb4eb53b
xy <damsinner@gmx.de>
2020-08-26 07:05:45 +0200

    better directory structure for untar files

--- tools/unTarProblematic.sh
+++ tools/unTarProblematic.sh
@@ -3,8 +3,12 @@ for file in *.tar
 do
 	echo ""
 	echo "Extracting $file ..."
-	pv "$file" | tar -x --ignore-failed-read --totals
-	# pv "$file" | tar -x --totals
+	cuttedName=$( basename $file .tar) # filename without extension
+	# note: use a slash to produce a directory at the end of $cuttedName
+	# note: first create the directory, otherwise tar complains
+	mkdir "$cuttedName"/
+	 pv "$file" | tar -x --ignore-failed-read --totals --directory "$cuttedName"/
+	# pv "$file" | tar -x --totals --direectory "$uncuttedName"/
 	result=$?
 	if [ $result -eq 0 ]; then
 		echo "File $file successfully extracted."

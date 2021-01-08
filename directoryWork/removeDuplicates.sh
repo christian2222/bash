5260a83ece685f0f2d194cb2ebc5fc61aaf0fa5b
xy <damsinner@gmx.de>
2021-01-08 22:47:00 +0100

    more notes

--- directoryWork/removeDuplicates.sh
+++ directoryWork/removeDuplicates.sh
@@ -2,5 +2,5 @@ while IFS= read -r line
 do
 	escapedLine=$(printf '%q' "$line")
 	echo " remove file $escapedLine"
-	rm "$escapedLine"
+	rm $escapedLine # rm -i does not work because of read
 done < filesToRemove.txt

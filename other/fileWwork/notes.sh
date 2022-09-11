53d0e3f6c2c699dadd550cc7e095859aea308fdc
xy <damsinner@gmx.de>
2022-09-11 20:31:47 +0200

    saveToTar moved to aliases

--- other/fileWwork/notes.sh
+++ other/fileWwork/notes.sh
@@ -1,11 +1,7 @@
 #! /bin/bash
 
-# unite both directories in one tar file:
-# tar cvf webArbeitDataSave$(date +"%m%d%y_%Hh-%Mm-%Ss").tar bootstrap-3.3.6-dist/ workspace/
-# mv *.tar ../saves/
-# ls ../saves/
 
-# note: use tree-d
+# note: use tree -d
 # find . -depth -exec ls -d {} \; > directories.txt
 # sed -i 's/.//' directories.txt # delete the points at the beginning; -i changes the file itself!
 

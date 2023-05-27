f7573c8bfebdb8b19228b8aa6fb890cf1b0b1aa4
xy <damsinner@gmx.de>
2023-05-27 12:29:57 +0200

    moved documentation

--- other/notes.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-
-
-# note: use tree -d
-# find . -depth -exec ls -d {} \; > directories.txt
-# sed -i 's/.//' directories.txt # delete the points at the beginning; -i changes the file itself!
-
